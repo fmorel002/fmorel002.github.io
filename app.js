9b58caeab8e9e0da5eecb6964e413c5dd5b16f4f
fmorel002 <flo.morel33@hotmail.fr>
2017-05-03 14:58:05 +0200

    service worker

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ this.onpush = function(event) {
   console.log(event.data);
   // From here we can write the data to IndexedDB, send it to any open
   // windows, display a notification, etc.
+  showNotification();
 }
 
 navigator.serviceWorker.register('/fmorel002.github.io/sw.js').then(

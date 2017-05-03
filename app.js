a7e8512507b13b4bbc38f79ee8e7a005d4a2af6c
fmorel002 <flo.morel33@hotmail.fr>
2017-05-03 14:43:39 +0200

    service worker

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+this.onpush = function(event) {
+  console.log(event.data);
+  // From here we can write the data to IndexedDB, send it to any open
+  // windows, display a notification, etc.
+}
+
+navigator.serviceWorker.register('/fmorel002.github.io/sw.js').then(
+  function(serviceWorkerRegistration) {
+    serviceWorkerRegistration.pushManager.subscribe().then(
+      function(pushSubscription) {
+        console.log(pushSubscription.endpoint);
+        // The push subscription details needed by the application
+        // server are now available, and can be sent to it using,
+        // for example, an XMLHttpRequest.
+      }, function(error) {
+        // During development it often helps to log errors to the
+        // console. In a production environment it might make sense to
+        // also report information about errors back to the
+        // application server.
+        console.log(error);
+      }
+    );
+  });
+
 // register service worker
 
-if ('serviceWorker' in navigator) {
+/*if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/fmorel002.github.io/sw.js', { scope: '/fmorel002.github.io/' }).then(function(reg) {
 
     if(reg.installing) {
@@ -71,4 +95,4 @@ window.onload = function() {
       console.log(Error);
     });
   }
-};
+};*/

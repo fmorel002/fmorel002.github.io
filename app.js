3bcd016b57be33ebe0f51057a05ae337ba169662
fmorel002 <flo.morel33@hotmail.fr>
2017-05-03 12:58:39 +0200

    service worker

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // register service worker
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
+  navigator.serviceWorker.register('/fmorel002.github.io/sw.js', { scope: '/fmorel002.github.io/' }).then(function(reg) {
 
     if(reg.installing) {
       console.log('Service worker installing');
